4d28c91dc0f954940a68930a54be640acae77c6e
Taylor Sarrafian <surprisetalk@gmail.com>
2016-07-20 16:33:33 +0000

    separated pagelets and pages

--- pagelet.js
+++ pagelet.js
@@ -1,4 +1,5 @@
 
+var _ = require('underscore');
 var piler = require('../aiport-dev/dev.js').pile;
 
 // TODO: can we get around this mongoose thing?
@@ -6,9 +7,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
-// TODO: population
-module.exports = piler( "pagelet", 
-                        { scrap: String,
+var schema = { scrap: String,
                           options: {}, 
                           pagelets: [ { type: Schema.Types.ObjectId, 
-                                        ref: "pagelet" } ] } );
+                                        ref: "pagelet" } ] };
+
+// TODO: this is slow
+var populator = model => pagelet_id =>
+    firster( model )( { "_id": pagelet_id } );
+
+var populatorer = model => pagelet =>
+    Promise.all( pagelet.pagelets.map( populator( model ) ) ).then( pagelets => _.extend( pagelet, { pagelets: pagelets } ) );
+
+var fetcher = model => data => 
+    model.find( data ).then( pagelets => Promise.all( pagelets.map( populatorer( model ) ) ) );
+
+var firster = model => data => 
+    model.findOne( data ).then( populatorer( model ) );
+
+var controllers = model => ({
+    fetch: fetcher( model ),
+    first: firster( model )
+});
+
+// TODO: population
+module.exports = piler( "pagelet", schema, controllers );
